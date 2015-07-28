f58584703232eae7b50fffa1d3879afbba50805e
Arthaey <arthaey@gmail.com>
2015-07-28 11:57:04 -0500

    Added basic tests for parsing arguments.

--- spec/bbc_progress_spec.rb
+++ spec/bbc_progress_spec.rb
@@ -34,23 +34,77 @@ RSpec.describe BBCodeProgress do
     end
   end
 
-
   context "parsing command-line arguments" do
-    it "parses item with leading --", :pending => true
-    it "parses item without leading --", :pending => true
-    it "parses item with value", :pending => true
-    it "parses item with max", :pending => true
-    it "parses item with value and max", :pending => true
-    it "parses item with fractional value", :pending => true
-    it "rejects item with no value", :pending => true
-    it "parses item with value greater than max", :pending => true
+    it "parses item with value" do
+      items = BBCodeProgress.parse_args(["foo=1"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 1, nil)
+    end
+
+    it "parses item with max", :pending => true do
+      items = BBCodeProgress.parse_args(["foo=/42"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", nil, 42)
+    end
+
+    it "parses item with value and max" do
+      items = BBCodeProgress.parse_args(["foo=1/42"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 1, 42)
+    end
+
+    it "parses item with fractional value", :pending => true do
+      items = BBCodeProgress.parse_args(["foo=.1"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 0.1, nil)
+    end
+
+    it "parses item with fractional value and int max", :pending => true do
+      items = BBCodeProgress.parse_args(["foo=.1/42"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 0.1, 42)
+    end
+
+    it "rejects item with fractional max", :pending => true do
+      expect{BBCodeProgress.parse_args(["foo=1/4.2"])}.to raise_error("ERROR: Cannot use fractional max")
+    end
+
+    it "rejects item with no value" do
+      items = BBCodeProgress.parse_args(["foo"])
+      expect(items.length).to eq(0)
+      items = BBCodeProgress.parse_args(["bar="])
+      expect(items.length).to eq(0)
+    end
+
+    it "parses item with value greater than max" do
+      items = BBCodeProgress.parse_args(["foo=42/37"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 42, 37)
+    end
+
+    it "parses multiple items" do
+      items = BBCodeProgress.parse_args(["foo=1/42", "bar=2/37"])
+      expect(items.length).to eq(2)
+      expect_progress(items["foo"], "foo", 1, 42)
+      expect_progress(items["bar"], "bar", 2, 37)
+    end
+
+    it "parses item with leading -- or -" do
+      items = BBCodeProgress.parse_args(["--foo=1"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 1, nil)
+
+      items = BBCodeProgress.parse_args(["-foo=1"])
+      expect(items.length).to eq(1)
+      expect_progress(items["foo"], "foo", 1, nil)
+    end
+
     it "parses item incrementing value", :pending => true
     it "parses item incrementing max", :pending => true
     it "parses item incrementing value and max", :pending => true
     it "parses item decrementing value", :pending => true
     it "parses item decrementing max", :pending => true
     it "parses item decrementing value and max", :pending => true
-    it "parses multiple items", :pending => true
   end
 
   context "parsing signature" do
@@ -58,18 +112,15 @@ RSpec.describe BBCodeProgress do
       sig = "[progress=foo]1/42[/progress]"
       items = BBCodeProgress.parse_sig(sig)
       expect(items.length).to eq(1)
-      expect(items.keys[0]).to eq("foo")
-
-      p = items["foo"]
-      expect_progress(p, "foo", 1, 42)
+      expect_progress(items["foo"], "foo", 1, 42)
     end
 
     it "parses multiple items" do
       sig = "[progress=foo]1/42[/progress] [progress=bar]2/37[/progress]"
       items = BBCodeProgress.parse_sig(sig)
       expect(items.length).to eq(2)
-      expect(items.keys[0]).to eq("foo")
-      expect(items.keys[1]).to eq("bar")
+      expect_progress(items["foo"], "foo", 1, 42)
+      expect_progress(items["bar"], "bar", 2, 37)
     end
   end
 
